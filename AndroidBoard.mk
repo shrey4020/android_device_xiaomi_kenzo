9fb87f72f098dd534afdc97fdf9abdd69e28a926
balika011 <balika011@gmail.com>
2016-06-15 01:10:23 +0200

    More files

---
+++ AndroidBoard.mk
@@ -0,0 +1,20 @@
+LOCAL_PATH := $(call my-dir)
+
+#----------------------------------------------------------------------
+# extra images
+#----------------------------------------------------------------------
+include build/core/generate_extra_images.mk
+
+
+#----------------------------------------------------------------------
+# Radio image
+#----------------------------------------------------------------------
+ifeq ($(ADD_RADIO_FILES), true)
+radio_dir := $(LOCAL_PATH)/radio
+RADIO_FILES := $(shell cd $(radio_dir) ; ls)
+$(foreach f, $(RADIO_FILES), \
+    $(call add-radio-file,radio/$(f)))
+
+INSTALLED_RADIOIMAGE_TARGET += $(TARGET_BOOTLOADER_EMMC_INTERNAL)
+endif
+
---
+++ releasetools.py
@@ -0,0 +1,60 @@
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import common
+import re
+import sha
+
+def FullOTA_Assertions(info):
+  pass
+
+def IncrementalOTA_Assertions(info):
+  pass
+
+image_partitions = {
+   'cmnlib.mbn'        : 'cmnlib',
+   'rpm.mbn'           : 'rpm',
+   'tz.mbn'            : 'tz',
+   'emmc_appsboot.mbn' : 'aboot',
+   'sbl1.mbn'          : 'sbl1',
+   'keymaster.mbn'     : 'keymaster',
+   'hyp.mbn'           : 'hyp',
+   'splash.img'        : 'splash',
+   'devinfo.img'       : 'devinfo',
+}
+
+def FullOTA_InstallEnd(info):
+  for img_name, partition in image_partitions.iteritems():
+    try:
+      img_file = info.input_zip.read("RADIO/" + img_name)
+      info.script.Print("update image " + img_name + "...")
+      common.ZipWriteStr(info.output_zip, 'firmware-update/' + img_name, img_file)
+      info.script.AppendExtra(('package_extract_file("firmware-update/' + img_name + '", "/dev/block/bootdevice/by-name/' + partition + '");'))
+    except KeyError:
+      print "warning: no " + img_name + " image in input target_files; not flashing " + img_name
+
+
+def IncrementalOTA_InstallEnd(info):
+  for img_name, partition in image_partitions.iteritems():
+    try:
+      source_file = info.source_zip.read("RADIO/" + img_name)
+      target_file = info.target_zip.read("RADIO/" + img_name)
+      if source_file != target_file:
+        common.ZipWriteStr(info.output_zip, 'firmware-update/' + img_name, target_file)
+        info.script.AppendExtra(('package_extract_file("firmware-update/' + img_name + '", "/dev/block/bootdevice/by-name/' + partition + '");'))
+      else:
+        print img_name + " image unchanged; skipping"
+    except KeyError:
+      print "warning: " + img_name + " image missing from target; not flashing " + img_name
+
